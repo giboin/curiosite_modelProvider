c99696d1f0dc1e22b9f7c162dc4d45b568e51bca
giboin <louis.giboin@imt-atlantique.net>
2022-04-29 12:37:52 +0200

    parametres et choix de moteur de recherche

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 
 import 'dart:async';
+import 'package:curiosite/parametres.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends MaterialApp {
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -22,207 +24,246 @@ class _MyAppState extends State<MyApp> {
   double progress = 0;
   Icon pcVersion=const Icon(Icons.check_box_outline_blank, color: Colors.black,);
   TextEditingController searchText = TextEditingController();
-  String searchEngine="lilo";
+  late String searchEngine;
+
+  Future<void> _getEngine() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('engine')) {
+      return;
+    }
+    setState(() {
+      searchEngine = prefs.getString('engine') ?? "erreur";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getEngine();
+  }
 
   @override
   Widget build(BuildContext context) {
     searchText.text=url;
     return MaterialApp(
         home: WillPopScope(
-          onWillPop: () async {
-            if (await _webViewController.canGoBack()){
-              _webViewController.goBack();
-              return false;
-            }
-            return true;
-          },
-          child: Scaffold(
-            appBar: AppBar(
-              leading:IconButton(
-                icon:const Icon(Icons.arrow_back),
-                onPressed: (){
-                  _webViewController.goBack();
-                },
-              ),
-              title: Container(
-                width: double.infinity,
-                height: 40,
-                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
-
-                child: Center(
-//=======================================================
-//                search bar
-//=======================================================
-                  child: TextField(
-                    controller: searchText,
-                    keyboardType: TextInputType.url,
-                    onSubmitted: (text){
-                      search(text);
+            onWillPop: () async {
+              if (await _webViewController.canGoBack()){
+                _webViewController.goBack();
+                return false;
+              }
+              return true;
+            },
+            child: Builder(builder:(context)=>Scaffold(
+              appBar: AppBar(
+                  leading:IconButton(
+                    icon:const Icon(Icons.arrow_back),
+                    onPressed: (){
+                      _webViewController.goBack();
                     },
-                    decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          icon: const Icon(Icons.clear),
-                          onPressed: () {
-                            searchText.text="";
-                          },
-                        ),
-                        prefixIcon: IconButton(
-                          icon: const Icon(Icons.search),
-                          onPressed: () {
-                            search(searchText.text);
-                          },
-                        ),
-                        hintText: 'Search...',
-                        border: InputBorder.none),
                   ),
-                ),
+                  title: Container(
+                    width: double.infinity,
+                    height: 40,
+                    decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
+
+                    child: Center(
+                      child: searchBar(),
+                    ),
+                  ),
+                  actions: <Widget>[
+                    const Icon(Icons.brightness_1_outlined),
+                    appbarMenu(context),
+                  ]
               ),
 
-//=======================================================
-//                menu
-//=======================================================
-
-              actions: <Widget>[
-                const Icon(Icons.brightness_1_outlined),
-                PopupMenuButton(
-                    itemBuilder: (BuildContext context){
-                      return [
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                ElevatedButton(
-                                    child:const Icon(Icons.arrow_forward),
-                                    onPressed: (){
-                                      _webViewController.goForward();
-                                    }
-                                ),
-                                MaterialButton(
-                                    child:const Icon(Icons.refresh),
-                                    onPressed: (){
-                                      _webViewController.reload();
-                                    }
-                                ),
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.add_moderator, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Nouvel onglet nav. priv.")
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.file_open, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Nouvel onglet expl. fichiers")
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.access_time, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Historique")
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.star, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Favoris")
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.search, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Rechercher sur la page")
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                          onTap: (){
-                            if (pcVersion.icon==Icons.check_box_outline_blank){
-                              pcVersion=const Icon(Icons.check_box, color: Colors.black,);
-                            }else{
-                              pcVersion=const Icon(Icons.check_box_outline_blank, color: Colors.black,);
-                            }
-                          },
-                            child: Row(
-                              children: [
-                                const Icon(Icons.computer, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Version pour ordi"),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                pcVersion,
-                              ],
-                            )
-                        ),
-                        PopupMenuItem(
-                            child: Row(
-                              children: [
-                                const Icon(Icons.settings, color: Colors.black,),
-                                Container(margin: const EdgeInsets.only(right: 20),),
-                                const Text("Paramètres")
-                              ],
-                            )
-                        ),
-                      ];
-                    }
-                )
-              ],
-            ),
 
 
 //=======================================================
 //                web View
 //=======================================================
-            body: Column(children: <Widget>[
-              Container(
-                  child: progress < 1.0
-                      ? LinearProgressIndicator(value: progress)
-                      : Container()),
-              Expanded(
-                child: InAppWebView(
-                  initialOptions: InAppWebViewGroupOptions(
-                      crossPlatform: InAppWebViewOptions()
+              body: Column(children: <Widget>[
+                Container(
+                    child: progress < 1.0
+                        ? LinearProgressIndicator(value: progress)
+                        : Container()),
+                Expanded(
+                  child: InAppWebView(
+                    initialOptions: InAppWebViewGroupOptions(
+                        crossPlatform: InAppWebViewOptions(
+                            preferredContentMode: pcVersion.icon==Icons.check_box_outline_blank? UserPreferredContentMode.MOBILE : UserPreferredContentMode.DESKTOP
+                        )
+                    ),
+                    onWebViewCreated: (InAppWebViewController controller) {
+                      _webViewController = controller;
+                      _webViewController.loadUrl(urlRequest: URLRequest(url:Uri(path: "flutter.dev/")));
+                    },
+                    onLoadStart: (controller, url) {
+                      setState(() {
+                        this.url = url.toString();
+                      });
+                    },
+                    onLoadStop: (controller, url) async {
+                      setState(() {
+                        this.url = url.toString();
+                      });
+                    },
+                    onProgressChanged: (InAppWebViewController controller, int progress) {
+                      setState(() {
+                        this.progress = progress / 100;
+                      });
+                    },
                   ),
-                  onWebViewCreated: (InAppWebViewController controller) {
-                    _webViewController = controller;
-                    _webViewController.loadUrl(urlRequest: URLRequest(url:Uri(path: "flutter.dev/")));
-                  },
-                  onLoadStart: (controller, url) {
-                    setState(() {
-                      this.url = url.toString();
-                    });
-                  },
-                  onLoadStop: (controller, url) async {
-                    setState(() {
-                      this.url = url.toString();
-                    });
-                  },
-                  onProgressChanged: (InAppWebViewController controller, int progress) {
-                    setState(() {
-                      this.progress = progress / 100;
-                    });
-                  },
                 ),
-              ),
-            ]),
-          ),
+              ]),
+            ),
+            )
         )
     );
   }
+
+  Widget appbarMenu(BuildContext context){
+    return PopupMenuButton(
+        onSelected: (result){
+          switch(result){
+            case 7: _openSettings(context); break;
+            case 1: break;
+          }
+        },
+        itemBuilder: (context)=>[
+          PopupMenuItem(
+              value:0,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                      child:const Icon(Icons.arrow_forward),
+                      onPressed: (){
+                        _webViewController.goForward();
+                      }
+                  ),
+                  MaterialButton(
+                      child:const Icon(Icons.refresh),
+                      onPressed: (){
+                        _webViewController.reload();
+                      }
+                  ),
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value: 1,
+              child: Row(
+                children: [
+                  const Icon(Icons.add_moderator, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Nouvel onglet nav. priv.")
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value: 2,
+              child: Row(
+                children: [
+                  const Icon(Icons.file_open, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Nouvel onglet expl. fichiers")
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value: 3,
+              child: Row(
+                children: [
+                  const Icon(Icons.access_time, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Historique")
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value: 4,
+              child: Row(
+                children: [
+                  const Icon(Icons.star, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Favoris")
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value: 5,
+              child: Row(
+                children: [
+                  const Icon(Icons.search, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Rechercher sur la page")
+                ],
+              )
+          ),
+          PopupMenuItem(
+              value:6,
+              onTap: (){
+                if (pcVersion.icon==Icons.check_box_outline_blank){
+                  pcVersion=const Icon(Icons.check_box, color: Colors.black,);
+                }else{
+                  pcVersion=const Icon(Icons.check_box_outline_blank, color: Colors.black,);
+                }
+              },
+              child: Row(
+                children: [
+                  const Icon(Icons.computer, color: Colors.black,),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  const Text("Version pour ordi"),
+                  Container(margin: const EdgeInsets.only(right: 20),),
+                  pcVersion,
+                ],
+              )
+          ),
+          PopupMenuItem(
+            value: 7,
+            child: Row(
+              children: [
+                const Icon(Icons.settings, color: Colors.black,),
+                Container(margin: const EdgeInsets.only(right: 20),),
+                const Text("Paramètres")
+              ],
+            ),
+          ),
+        ]
+
+    );
+  }
+
+  Future<void> _openSettings(BuildContext ctx) async {
+    await Navigator.push(ctx, MaterialPageRoute(builder: (context) => Parametres())).then((value) => _getEngine());
+  }
+
+
+  Widget searchBar(){
+    return TextField(
+      controller: searchText,
+      keyboardType: TextInputType.url,
+      onSubmitted: (text){
+        search(text);
+      },
+      decoration: InputDecoration(
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              searchText.text="";
+            },
+          ),
+          prefixIcon: IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              search(searchText.text);
+            },
+          ),
+          hintText: 'Search...',
+          border: InputBorder.none),
+    );
+  }
+
 
   String removeHttp(String str){
     if (str.length>=7 && str.substring(0, 7)=='http://') {
@@ -243,7 +284,7 @@ class _MyAppState extends State<MyApp> {
 
   String formatToUrl(String str){
     RegExp exp = RegExp(r"^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.?)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$|^((\\(\\[^\s\\]+)+|([A-Za-z]:(\\)?|[A-z]:(\\[^\s\\]+)+))(\\)?)$");
-    if(exp.firstMatch(str)!=null){
+    if(exp.firstMatch(str)!=null && (str.contains(".") || str.contains("/"))){
       return str;
     }
     if(searchEngine=="google"){
@@ -254,4 +295,5 @@ class _MyAppState extends State<MyApp> {
     }
     return str;
   }
+
 }
---
+++ lib/parametres.dart
@@ -0,0 +1,78 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Parametres extends StatefulWidget {
+  Parametres({Key? key}) : super(key: key);
+
+  @override
+  _ParamState createState() => _ParamState();
+}
+
+class _ParamState extends State<Parametres> {
+
+  late String _engine;
+
+  Future<void> _getEngine() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('engine')) {
+      return;
+    }
+    setState(() {
+      _engine = prefs.getString('engine') ?? "erreur";
+    });
+  }
+
+  Future<void> _saveEngine(String str) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('engine', str);
+    _getEngine();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getEngine();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Paramètres"),),
+        body: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: 2,
+            separatorBuilder: (BuildContext context,
+                int index) => const Divider(height: 3, color: Colors.grey),
+            itemBuilder: (BuildContext context, int index) {
+              return PopupMenuButton(
+                  child: ListTile(title: [
+                    Text("moteur de recherche: "+(_engine)),
+                    Text("autre chose"),
+                  ][index],),
+                  itemBuilder: (BuildContext context)=><List<PopupMenuEntry>>[
+                    [
+                      PopupMenuItem(
+                        child: Text("Google"),
+                        onTap: (){_saveEngine("google");},
+                      ),
+                      PopupMenuItem(
+                        child: Text("Lilo"),
+                        onTap: (){_saveEngine("lilo");},
+                      ),
+                    ],
+
+                    []
+                  ][index]);
+            }
+        )
+    );
+  }
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
